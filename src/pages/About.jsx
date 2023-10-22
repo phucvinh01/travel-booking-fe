44d1a3e738e97e25d9df0da3c5fa0c3140021a1a
VNDT-082 <vonguyenduytan08022002@gmail.com>
2023-10-22 22:02:55 +0700

    duytan22_10_2023

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+
+//import * as React from 'react';
+import React from 'react';
 
 const About = () => {
     return (
-        <><main></main></>
+        <><main>
+            <div className='row' style={{marginTop:"15%"}}>
+                <div className='col-md-12'>
+                    <h1>Nhóm 6: QUẢN LÝ TOUR DU LỊCH</h1>
+                    <h6>Thành viên</h6>
+                </div>
+                <div className='col-md-3'>
+                    <div className='card'>
+                        <img src='..//..//src/Images/person/man.jpg'/>
+                        <div className='card-body'>
+                            <p><strong>VÕ NGUYỄN DUY TÂN</strong></p>
+                            <p>MSSV: 2001200082</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='card'>
+                        <img src='..//..//src/Images/person/man.jpg'/>
+                        <div className='card-body'>
+                            <p><strong>NGUYỄN PHÚC VINH</strong></p>
+                            <p>MSSV: 2001200636</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='card'>
+                        <img src='..//..//src/Images/person/man.jpg'/>
+                        <div className='card-body'>
+                            <p><strong>VŨ NGỌC HOÀNG CUNG</strong></p>
+                            <p>MSSV: 2001206991</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='card'>
+                        <img src='..//..//src/Images/person/man.jpg'/>
+                        <div className='card-body'>
+                            <p><strong>PHẠM HẢI ĐĂNG</strong></p>
+                            <p>MSSV: 2001202029 </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </main></>
     )
 }
 
