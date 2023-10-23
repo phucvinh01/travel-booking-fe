b5645d80f113b6a6d6b654104f1ca62573451957
phucvinh01 <nguyenphucvinh1920@gmail.com>
2023-10-23 20:12:46 +0700

    23/10

--- src/components/ModalEditTour/index.jsx
+++ src/components/ModalEditTour/index.jsx
@@ -90,49 +90,49 @@ const ModalEditTour = (props) => {
     return (
         <>
 
-            <Modal footer={null} width={1000} title="Chỉnh sửa" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <Modal footer={ null } width={ 1000 } title="Chỉnh sửa" open={ isModalOpen } onOk={ handleOk } onCancel={ handleCancel }>
                 <div className='row g-2'>
                     <div className='col-lg-3 col-md-12 col-sm-12'>
                         <div className='mb-3'>
                             <label
-                                htmlFor={id + '-name'}
+                                htmlFor={ id + '-name' }
                                 className='form-label fw-bolder'>
                                 Tên tour
                             </label>
                             <input
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
+                                value={ name }
+                                onChange={ (e) => setName(e.target.value) }
                                 type='text'
                                 className='form-control'
-                                id={id + '-name'}></input>
+                                id={ id + '-name' }></input>
                         </div>
                         <div className='mb-3'>
                             <label
-                                htmlFor={id + '-price'}
+                                htmlFor={ id + '-price' }
                                 className='form-label fw-bolder'>
                                 Giá
                             </label>
                             <input
-                                value={price}
-                                onChange={(e) => setPrice(e.target.value)}
+                                value={ price }
+                                onChange={ (e) => setPrice(e.target.value) }
                                 type='number'
                                 className='form-control'
-                                id={id + '-price'}></input>
+                                id={ id + '-price' }></input>
                         </div>
                         <div className='mb-3'>
                             <label
-                                htmlFor={id + '-category'}
+                                htmlFor={ id + '-category' }
                                 className='form-label fw-bolder'>
                                 Loại tour
                             </label>
                             <Select
-                                value={findNameCateById(cate, category)}
+                                value={ findNameCateById(cate, category) }
                                 size='large'
-                                onChange={onChange}
+                                onChange={ onChange }
                                 id='category'
-                                style={{
+                                style={ {
                                     width: '100%',
-                                }}
+                                } }
                                 allowClear
                                 options={
                                     cate && cate.length > 0 && cate.map((item, index) => {
@@ -152,27 +152,27 @@ const ModalEditTour = (props) => {
                     <div className='col-lg-3 col-md-12 col-sm-12'>
                         <div className='mb-3'>
                             <label
-                                htmlFor={id + '-brand'}
+                                htmlFor={ id + '-brand' }
                                 className='form-label fw-bolder'>
                                 Chuyến bay
                             </label>
                             <Select
-                                value={findNameFlightById(flight, idTranport)}
+                                value={ findNameFlightById(flight, idTranport) }
                                 size='large'
-                                onChange={onChangeIdTranpost}
+                                onChange={ onChangeIdTranpost }
                                 id='brand'
-                                style={{
+                                style={ {
                                     width: '100%',
-                                }}
+                                } }
                                 allowClear
                                 placeholder="Search to Select"
                                 showSearch
                                 optionFilterProp="children"
-                                filterOption={(input, option) => (option?.label ?? '').includes(input)}
-                                filterSort={(optionA, optionB) =>
+                                filterOption={ (input, option) => (option?.label ?? '').includes(input) }
+                                filterSort={ (optionA, optionB) =>
                                     (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
                                 }
-                                options={flight && flight.length > 0 && flight.map((item, index) => {
+                                options={ flight && flight.length > 0 && flight.map((item, index) => {
                                     return (
                                         {
                                             value: item.idChuyenBay,
@@ -180,66 +180,66 @@ const ModalEditTour = (props) => {
                                             key: item.idChuyenBay
                                         }
                                     )
-                                })}
+                                }) }
                             />
                         </div>
                         <div className='mb-3'>
                             <label
-                                htmlFor={id + '-description'}
+                                htmlFor={ id + '-description' }
                                 className='form-label fw-bolder'>
                                 Mô tả
                             </label>
                             <TextArea
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                                style={{
+                                value={ description }
+                                onChange={ (e) => setDescription(e.target.value) }
+                                style={ {
                                     height: 123,
                                     resize: 'none',
-                                }}
+                                } }
                                 type='text'
                                 className='form-control'
-                                id={id + '-description'}></TextArea>
+                                id={ id + '-description' }></TextArea>
                         </div>
                     </div>
                     <div className='col-lg-3 col-md-12 col-sm-12'>
                         <div className='mb-3'>
                             <label
-                                htmlFor={id + '-createAt'}
+                                htmlFor={ id + '-createAt' }
                                 className='form-label fw-bolder'>
                                 Ngày tạo
                             </label>
                             <input
-                                defaultValue={createAt}
-                                value={createAt}
-                                onChange={(e) => setCreateAt(e.target.value)}
+                                defaultValue={ createAt }
+                                value={ createAt }
+                                onChange={ (e) => setCreateAt(e.target.value) }
                                 type='date'
                                 className='form-control'
-                                id={id + '-createAt'}></input>
+                                id={ id + '-createAt' }></input>
                         </div>
                         <div className='mb-3'>
                             <label
-                                htmlFor={id + '-hotel'}
+                                htmlFor={ id + '-hotel' }
                                 className='form-label fw-bolder'>
                                 Khách sạn
                             </label>
                             <Select
-                                value={findNameHotelById(hotel, idHotel)}
+                                value={ findNameHotelById(hotel, idHotel) }
 
                                 size='large'
-                                onChange={onChangeHotel}
+                                onChange={ onChangeHotel }
                                 id='hotel'
-                                style={{
+                                style={ {
                                     width: '100%',
-                                }}
+                                } }
                                 placeholder="Search to Select"
                                 allowClear
                                 showSearch
                                 optionFilterProp="children"
-                                filterOption={(input, option) => (option?.label ?? '').includes(input)}
-                                filterSort={(optionA, optionB) =>
+                                filterOption={ (input, option) => (option?.label ?? '').includes(input) }
+                                filterSort={ (optionA, optionB) =>
                                     (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
                                 }
-                                options={hotel && hotel.length > 0 && hotel.map((item, index) => {
+                                options={ hotel && hotel.length > 0 && hotel.map((item, index) => {
                                     return (
                                         {
                                             value: item.idKhachSan,
@@ -247,25 +247,25 @@ const ModalEditTour = (props) => {
                                             key: item.idKhachSan
                                         }
                                     )
-                                })}
+                                }) }
                             />
                         </div>
                         <div className='mb-3'>
                             <label
-                                htmlFor={id + '-tranpost'}
+                                htmlFor={ id + '-tranpost' }
                                 className='form-label fw-bolder'>
                                 Phương tiện di chuyển
                             </label>
                             <Select
-                                value={tranpost}
+                                value={ tranpost }
                                 size='large'
-                                onChange={onChangeTranpost}
+                                onChange={ onChangeTranpost }
                                 id='tranpost'
-                                style={{
+                                style={ {
                                     width: '100%',
-                                }}
+                                } }
                                 allowClear
-                                options={[
+                                options={ [
                                     {
                                         value: "Xe",
                                         key: 1
@@ -274,19 +274,19 @@ const ModalEditTour = (props) => {
                                         value: "Máy bay",
                                         key: 2
                                     }
-                                ]}
+                                ] }
                             />
                         </div>
 
                     </div>
                     <div className='col-lg-3 col-md-12 col-sm-12'>
                         <div className='p-0 mb-3'>
-                            <Upload setImage={setImage} value={image} />
+                            <Upload setImage={ setImage } value={ image } />
                         </div>
                     </div>
                 </div>
                 <div className='d-flex justify-content-end'>
-                    <Button type='primary'  onClick={() => handleClick()}>{loading ? <Spin></Spin> : <>Update</>} </Button>
+                    <Button type='primary' disabled={ !isFull ? true : false } onClick={ () => handleClick() }>{ loading ? <Spin></Spin> : <>Create</> } </Button>
                 </div>
             </Modal>
         </>
